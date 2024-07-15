2810aa15b317ea3ad2f623f66e3c3ef4fc360a19
slowy07 <slowy.arfy@gmail.com>
2024-07-15 17:27:44 +0700

    chore: update uefi
    Signed-off-by: slowy07 <slowy.arfy@gmail.com>

--- src/boot/uefi.asm
+++ src/boot/uefi.asm
@@ -117,6 +117,150 @@ EntryPoint:
   shl rcx, 3
   mov rsi, [CONFIG]
 
+nextentry:
+  dec rcx
+  cmp rcx, 0
+  je error
+  mov rdx, [ACPI_TABLE_GUID]
+  lodsq
+  cmp rax, rdx
+  jne nextentry
+  mov rdx, [ACPI_TABLE_GUID + 8]
+  lodsq
+  cmp rax, rdx
+  jne nextentry
+  mov [ACPI], rax
+
+  mov rcx, EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID
+  mov rdx, 0
+  mov r8, VIDEO
+  mov rax, [BS]
+  mov rax, [rax + EFI_BOOT_SERVICES_LOCATEPROTOCOL]
+  call rax
+  cmp rax, EFI_SUCCESS
+  jne error
+
+  mov rcx, [VIDEO]
+  add rcx, EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE
+  mov rcx, [rcx]
+  mov rax, [rcx + 24]
+  mv [FB], rax
+  mov rax, [rcx + 32]
+  mov [FBS], rax
+  mov rcx, [rcx + 8]
+  
+  mov eax, [rcx + 4]
+  mov [HR], rax
+  mov eax, [rcx + 8]
+  mov [VR], rax
+  mov eax, [rcx + 32]
+  mv [PPSL], rax
+
+  mov rsi, PAYLOAD
+  mov rdi, 0x8000
+  mov rcx, 61440
+  rep movsb
+  mov ax, [0x8006]
+  cmp ax, 0x3436
+  jne sig_fail
+
+get_memmap:
+  mov rcx, [OUTPUT]
+  lea rdx, [msg_OK]
+  call [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL_OUTPUTSTRING]
+  
+  lea rcx, [memmapsize]
+  mov rdx, [memmap]
+  lea r8, [memmapkey]
+  lea r9, [memmapdescsize]
+  lea r10, [memmapdescver]
+  mov [rsp+32], r10
+  mov rax, [BS]
+  call [rax + EFI_BOOT_SERVICES_GETMEMORYMAP]
+  cmp al, EFI_BUFFER_TOO_SMALL
+  je get_memmap
+  cmp rax, EFI_SUCCESS
+  jne exitfailure
+  
+  mov rcx, [EFI_IMAGE_HANDLE]
+  mov rdx, [memmapkey]
+  mov rax, [BS]
+  call [rax + EFI_BOOT_SERVICE_EXITBOOTSERVICES]
+  cmp rax, EFI_SUCCESS
+  jne get_memmap
+
+  cli
+
+  mv rdi, 0x00005F00
+  mov rax, [FB]
+  stosq
+  mov rax, [FBS]
+  stosq
+  mov rax, [HR]
+  stosw
+  mov rax, [VR]
+  stosw
+  mov rax, [PPSL]
+  mv rax, [memmap]
+  stosq
+  mov rax, [memmapsize]
+  stosq
+  mov rax, [memmapkey]
+  stosq
+  mov rax, [memmapdescsize]
+  stosq
+  mov rax, [memmapdescver]
+  stosq
+
+  ; setting screen to black before jumpe to haley kernel
+  mov rdi, [FB]
+  mov eax, 0x00000000
+  mov rcx, [FBS]
+  shr rcx, 2
+  rep stosd
+
+  ; clear register
+  xor eax, eax
+  xor ecx, ecx
+  xor edx, edx
+  xor ebx, ebx
+  mov rsp, 0x8000
+  xor ebp, ebp
+  xor esi, esi
+  xor edi, edi
+  xor r8, r8
+  xor r9, r9
+  xor r10, r10
+  xor r11, r11
+  xor r12, r12
+  xor r13, r13
+  xor r14, r14
+  xor r15, r15
+  
+  mov bl, 'U'
+  jmp 0x8000
+
+exitfailure:
+  mov rdi, [FB]
+  mov eax, 0x00FF0000
+  mov rcx, [FBS]
+  shr rcx, 2
+  rep stosd
+
+error:
+  mov rcx, [OUTPUT]
+  lead rdx, [msg_error]
+  call [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL_OUTPUTSTRING]
+  jmp halt
+
+sig_fail:
+  mov rcx, [OUTPUT]
+  lea rdx, [msg_SigFaile]
+  call [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL_OUTPUTSTRING]
+halt:
+  halt
+  jmp halt
+
 EFI_SUCCESS equ 0
 EFI_LOAD_ERROR equ 1
 EFI_INVALID_PARAMETER equ 2
@@ -134,3 +278,7 @@ EFI_MEDIA_CHANGED equ 13
 EFI_NOT_FOUND equ 14
 
 EFI_RUNTIME_SERVICE_RESETSYSTEM equ 104
+
+EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL_OUTPUTSTRING equ 8
+EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL_SET_ATTRIBUTE equ 40
+EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL_CLEAR_SCREEN equ 48
